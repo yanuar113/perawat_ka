06912d1e1316ac650441317203dcc395b992ea74
gorengan11 <rehansmart2003@gmail.com>
2023-08-21 15:04:55 +0700

    id jereta

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kategori_checksheet;
 use Illuminate\Http\Request;
 
 class KategoriController extends Controller
 {
     public function getAll(){
         $authuser = auth()->user();
-        return ResponseController::customResponse(true, 'Berhasil mendapakan Kategori!', $authuser);
+        $categories = Kategori_checksheet::where('id_kereta', $authuser->id);
+        return ResponseController::customResponse(true, 'Berhasil mendapakan Kategori!', $categories);
     }
 }
