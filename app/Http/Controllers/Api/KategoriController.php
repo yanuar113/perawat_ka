de56a920d6beb40c5019ba3ae725faaafce61c92
gorengan11 <rehansmart2003@gmail.com>
2023-08-21 14:48:04 +0700

    auth user

---
+++ app/Http/Controllers/Api/KategoriController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class KategoriController extends Controller
+{
+    public function getAll()[
+        $authuser = auth()->user();
+        return ResponseController::customResponse(true, 'Berhasil mendapakan Kategori!', $authuser);
+    ]
+}
