39505e8dc29558710bef8f27ad04a4c3652b05a5
ncip48 <mbahcip00@gmail.com>
2024-04-23 15:12:24 +0700

    dd

--- app/Http/Controllers/Api/ChecksheetController.php
+++ app/Http/Controllers/Api/ChecksheetController.php
@@ -75,7 +75,7 @@ class ChecksheetController extends Controller
         $file->move(public_path('foto'), $filename);
 
         // read image from file system
-        $absolute_path = realpath('foto/' . $filename);
+        $absolute_path = './foto/' . $filename;
         $image = ImageManager::gd()->read($absolute_path);
 
         $watermarkText = $request->date;
@@ -110,7 +110,7 @@ class ChecksheetController extends Controller
         });
 
         // save modified image in new format
-        $absolute_path_save = realpath('foto/' . $mark);
+        $absolute_path_save = './foto/' . $mark;
         $image->toPng()->save($absolute_path_save);
 
         unlink(public_path('foto/' . $filename));
