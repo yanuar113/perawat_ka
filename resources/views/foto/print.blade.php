71d33aaa195dc02f82a5349300509d5fce88f6fa
ncip48 <mbahcip00@gmail.com>
2024-04-23 15:05:01 +0700

    checksheet

--- resources/views/foto/print.blade.php
+++ resources/views/foto/print.blade.php
@@ -9,7 +9,7 @@
         }
 
         body {
-            margin-top: 3cm;
+            margin-top: 2cm;
             margin-left: 1cm;
             margin-right: 1cm;
             margin-bottom: 1cm;
@@ -22,11 +22,11 @@
         }
 
         header {
-            font-size: 24px;
+            font-size: 22px;
             font-weight: bold;
             text-align: center;
             position: fixed;
-            top: 1cm;
+            top: 0.5cm;
             left: 1cm;
             right: 1cm;
         }
@@ -46,11 +46,14 @@
 
         .photo {
             border: 1px solid black;
-            padding: 8px;
-            margin-top: 0.7px;
+            padding: 18px;
+            /* margin-top: 0.7px;
             margin-left: 8rem;
-            margin-right: 8rem;
+            margin-right: 8rem; */
             text-align: center;
+            width: 355px;
+            alignt-self: center;
+            margin: auto;
         }
 
         .date {
@@ -75,7 +78,7 @@
                     $gambar = base64_encode($gambar);
                     $gambar = 'data:image/jpeg;base64,' . $gambar;
                 @endphp
-                <img src="{{ $gambar }}" alt="{{ $item->nama_item }}" width="100%" height="250"
+                <img src="{{ $gambar }}" alt="{{ $item->nama_item }}" width="340px" height="265px"
                     style="object-fit: fill">
             </div>
             <p>{{ $item->nama_item }}</p>
